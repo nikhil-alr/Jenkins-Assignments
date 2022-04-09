3b9ea6761c31b958448ba78410e8d7cbea0b218b
Nikhil Kamboj <nkhlkamboj@gmail.com>
2022-04-09 09:06:36 +0530

    App created

---
+++ demo/src/main/java/com/example/demo/TestUserC.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TestUserC {
+
+    @GetMapping("/Hello")
+    public String hello()
+    {
+        return "hello";
+    }
+
+    List<User> users = new ArrayList<>();
+
+    @PostMapping("/saveUser")
+    public ResponseEntity<User> saveUser(@RequestBody User user)
+    {
+        users.add(user);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/getUsers")
+    public ResponseEntity<List<User>> getAllUsers()
+    {
+        return ResponseEntity.ok(users);
+    }
+
+
+}
---
+++ demo/src/main/java/com/example/demo/User.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class User {
+
+    public String name;
+    public int age;
+    public String gender;
+
+}
