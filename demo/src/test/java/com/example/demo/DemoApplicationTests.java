f5d13a0bf9879a59ecf4694ee4fb7a6fe91f9364
Nikhil Kamboj <nkhlkamboj@gmail.com>
2022-04-09 13:18:49 +0530

    artifactory added

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(10,10);
 	}
 
 }
